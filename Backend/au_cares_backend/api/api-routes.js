a2f88efa05d9bd668880c76f91fa52b8bcfbba05
Anthony Baron <antonbaron10@gmail.com>
2021-03-16 01:51:13 -0400

    More fun

--- Backend/au_cares_backend/api/api-routes.js
+++ Backend/au_cares_backend/api/api-routes.js
@@ -6,10 +6,16 @@ const authentication = require('../services/authentication');
 const express = require("express"); //import express
 const app = express(); //used to set up an express server
 
+//Test status of server
+app.get('/ping', (req, res) => {
+    res.status(200).send("pong");
+})
+
+
 app.post('/createAccount', (req, res) => {
-    let userEmail = req.userEmail;
+    let userEmail = req.body.userEmail;
     if (userEmail.substr(userEmail.length - 21) === "@student.american.edu") {
-        let userPass = req.password;
+        let userPass = req.body.password;
         let accessCode = authentication.createUserAccount(userEmail, userPass);
     } else {
         res.sendStatus(401);
@@ -21,3 +27,6 @@ app.post('/createAccount', (req, res) => {
 app.post('/login', (req, res) => {
     authentication.login();
 });
+
+
+app.listen(3000);
