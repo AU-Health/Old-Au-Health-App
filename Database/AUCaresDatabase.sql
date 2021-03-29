523051a394a14bffbd470a1b3ac2db8ee8cf1725
Anthony Baron <antonbaron10@gmail.com>
2021-03-29 16:39:06 -0400

    DBchanges

--- Database/AUCaresDatabase.sql
+++ Database/AUCaresDatabase.sql
@@ -337,6 +337,20 @@ VALUES (2, 'Failed to Complete');
 INSERT INTO ActivityCompletedTypes(ActivityCompletedTypeId, ActivityCompletedTypeName)
 VALUES (3, 'No Attempt');
 
+CREATE TABLE `QuestionTypes`
+(
+    `QuestionTypeId` TINYINT(1),
+    `QuestionType`   VARCHAR(30),
+    PRIMARY KEY (QuestionTypeId)
+);
+INSERT INTO QuestionTypes(QuestionTypeId, QuestionType)
+VALUES (1, 'Multiple Choice');
+INSERT INTO QuestionTypes(QuestionTypeId, QuestionType)
+VALUES (2, 'Checkmark');
+INSERT INTO QuestionTypes(QuestionTypeId, QuestionType)
+VALUES (1, 'Text');
+
+
 
 ##########################################TruthsDaresQuestions######################
 
@@ -372,6 +386,8 @@ CREATE TABLE IF NOT EXISTS `Dares`
 CREATE TABLE IF NOT EXISTS `Questions`
 (
     `QuestionId`      SMALLINT UNSIGNED   NOT NULL UNIQUE AUTO_INCREMENT,
+    `QuestionTitle`   VARCHAR(50),
+    `QuestionTypeId`    TINYINT(1),
     `Question`        VARCHAR(500) UNIQUE NOT NULL,
     `Points`          TINYINT UNSIGNED    NOT NULL CHECK ( Points > 0 ),
     `CategoryId`      TINYINT(1)          NOT NULL,
@@ -379,7 +395,29 @@ CREATE TABLE IF NOT EXISTS `Questions`
     `HoursToComplete` DECIMAL(5, 1)       NOT NULL CHECK ( HoursToComplete > 0 ),
     `SentNum`         MEDIUMINT UNSIGNED  NOT NULL CHECK (SentNum >= 0),
     `CompleteNum`     MEDIUMINT UNSIGNED  NOT NULL CHECK ( CompleteNum >= 0 ),
-    PRIMARY KEY (QuestionId)
+    PRIMARY KEY (QuestionId),
+    FOREIGN KEY (QuestionTypeId) REFERENCES QuestionTypes (QuestionTypeId)
+);
+
+#Table to store response choices, for multiple choice, checkmarks
+CREATE TABLE `QuestionResponseChoices`
+(
+    `ResponseChoiceId` MEDIUMINT UNSIGNED,
+    `QuestionId`       SMALLINT UNSIGNED NOT NULL UNIQUE,
+    `ResponseChoice`   VARCHAR(50),
+    PRIMARY KEY (ResponseChoiceId),
+    FOREIGN KEY (QuestionId) REFERENCES Questions (QuestionId) ON DELETE CASCADE
+);
+
+#Table to store the answers to the choices
+CREATE TABLE `QuestionResponseAnswer`
+(
+    `ResponseAnswerId`        MEDIUMINT UNSIGNED,
+    `QuestionId`              SMALLINT UNSIGNED NOT NULL UNIQUE,
+    `CorrectResponseChoiceId` MEDIUMINT UNSIGNED,
+    PRIMARY KEY (ResponseAnswerId),
+    FOREIGN KEY (QuestionId) REFERENCES Questions (QuestionId),
+    FOREIGN KEY (QuestionId) REFERENCES Questions (QuestionId)
 );
 
 #All Truth Responses
