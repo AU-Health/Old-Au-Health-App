c7d4746e283b36adf4f00ff3bd8f775cebf99a47
Nathan Kennedy <nathankennedy122@gmail.com>
2021-04-22 13:45:36 -0400

    added mood.png and mood screen (after questionnaire screen). added text variables to feedback screen to make implementing functionality smoother.

--- Frontend/screens/onboarding/Onboarding_2.jsx
+++ Frontend/screens/onboarding/Onboarding_2.jsx
@@ -31,7 +31,7 @@ const Onboarding_2 = ({ navigation }) => {
                 </TouchableOpacity>
                 <TouchableOpacity
                     onPress={() => {
-                        navigation.navigate("Login");
+                        navigation.navigate("Mood");
                     }} style={styles.onboardingBtnContainer}>
                     <Text style={styles.onboardingBtn}>Continue</Text>
                 </TouchableOpacity>
